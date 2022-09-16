b549846de32fa6bde528dfa44ab4cdfae1b35456
CrimsonCoder42 <nostro37@gmail.com>
2022-09-16 10:10:35 -0400

    workbook 6.1

--- Java-Bootcamp-Resources-main/section6/IntroToArrays.java
+++ Java-Bootcamp-Resources-main/section6/IntroToArrays.java
@@ -1,5 +1,7 @@
 public class IntroToArrays {
     public static void main(String[] args) {
-        
+        String[] kingdoms = {"Mercia", "Wessex", "Northumbria", "EA"};
+
+        System.out.println(kingdoms[0]);
     }
 }
