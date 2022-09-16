22a0f6f38bfa4420ee07d0acb107410c411c3fff
CrimsonCoder42 <nostro37@gmail.com>
2022-09-16 10:35:22 -0400

    WBK 6.2

---
+++ Java-Bootcamp-Resources-main/section6/ArrayAndLoops.java
@@ -0,0 +1,10 @@
+public class ArrayAndLoops{
+    public static void main(String[] args) {
+        String[] kingdoms = {"Mercia", "Wessex", "Northumbria", "EA"};
+
+        for(int i = 0; i < kingdoms.length; i++) {
+            System.out.println(kingdoms[i]);
+        }
+
+    }
+}
