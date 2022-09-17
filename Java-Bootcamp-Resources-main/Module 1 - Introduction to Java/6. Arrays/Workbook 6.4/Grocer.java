625021c3359c407172fac80e4684a5007fe1dccb
CrimsonCoder42 <nostro37@gmail.com>
2022-09-17 16:41:33 -0400

    6.4 solved

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.4/Grocer.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.4/Grocer.java
@@ -1,11 +1,14 @@
+import java.util.Scanner;
+
 public class Grocer {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
         String[] store = {"apples", "bananas", "candy", "chocolate", "coffee", "tea"};
         System.out.println("\nWelcome to Java Grocers. ");
         System.out.println("What can I help you find?\n");
         //  Task 1: Set up Scanner, and pick up the user's response.  
-
+        String response = scan.nextLine();
         
         for (int i = 0; i < store.length; i++) {
           /* Task 2  
@@ -14,6 +17,12 @@ public class Grocer {
                     break;
                 }
           */
+          String option = store[i];
+          if(response.equals(option)) {
+            System.out.println("We have that in aisle " + i);
+              break;
+          }
+          
         }
 
     }
