cc23a40ef36cca1ebdbe54341f43a91e59f4c0b0
CrimsonCoder42 <nostro37@gmail.com>
2022-09-20 07:36:57 -0400

    6.11 done

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.11/Weather.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.11/Weather.java
@@ -1,13 +1,18 @@
 import java.util.Arrays;
+import java.text.DecimalFormat;
 
 public class Weather {
+    
     public static void main(String[] args) {
         
         double[] celsius = {12.5, 14.5, 17.0, 21.0, 23.0, 18.5, 20.0};
         double[] fahrenheit;
         //Task 2, call celsiusToFahrenheit and store the result in the fahrenheit array. 
+        fahrenheit = celciusToFahrenheit(celsius);
 
         //Task 4, Call printTemperatures for celsius and fahrenheit.
+        printTemperatures(celsius, "celsius");
+        
     }
 
 
@@ -23,6 +28,14 @@ public class Weather {
      *    4. return the fahrenheit array.
      */   
 
+     public static double[] celciusToFahrenheit(double[] celsius) {
+        double[] fahrenheit = Arrays.copyOf(celsius, celsius.length);
+        for(int i = 0; i < fahrenheit.length; i++) {
+            fahrenheit[i] = (fahrenheit[i] * 1.8) + 32;
+        }
+        return fahrenheit;
+     }
+
     /** Task 3
      * Function name - printTemperatures
      * @param temp ( double[] )
@@ -34,6 +47,12 @@ public class Weather {
      *   2. Adds a new line of space after printing the contents of the array.
      *   3. (Task 5) Rounds every temperature value to two decimal places. 
      */
-
+     public static void printTemperatures(double[] temp, String type) {
+        System.out.print(type + ": ");
+        for (int i = 0; i < temp.length; i++) {
+                System.out.print(String.format("%.2f", temp[i]) + " ");
+        }
+        System.out.print("\n");
+     }
 
 }
