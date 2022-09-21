2e3bb28ebbfd7732eeb13fbb59d0e5e1ed2275f5
CrimsonCoder42 <nostro37@gmail.com>
2022-09-21 06:42:50 -0400

    6.13 done

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.13/Prices.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.13/Prices.java
@@ -1,10 +1,37 @@
+import java.util.Arrays;
+
 public class Prices {
     public static void main(String[] args) {
         //Task 1 - Create a 2D array that can store 3 rows and 5 columns of double values.
 
+        double[][] pricingTable = new double[3][5];
+        
+
         // Task 2 - Populate your 2D array with values from the table (see article)
 
+        pricingTable[0][0] = 12.99;
+        pricingTable[0][1] = 8.99;
+        pricingTable[0][2] = 9.99;
+        pricingTable[0][3] = 10.49;
+        pricingTable[0][4] = 11.99;
+
+        pricingTable[1][0] = 0.99;
+        pricingTable[1][1] = 1.99;
+        pricingTable[1][2] = 2.49;
+        pricingTable[1][3] = 1.49;
+        pricingTable[1][4] = 2.99;
+
+        pricingTable[2][0] = 8.99;
+        pricingTable[2][1] = 7.99;
+        pricingTable[2][2] = 9.49;
+        pricingTable[2][3] = 9.99;
+        pricingTable[2][4] = 10.99;
+
+
         // Task 3 - Print the prices for each department. See the article for the expected output.  
+        System.out.println("Baking: " + Arrays.toString(pricingTable[0]));
+        System.out.println("Beverage: " + Arrays.toString(pricingTable[1]));
+        System.out.println("Cereals: " + Arrays.toString(pricingTable[2]));
 
     }
 }
--- Java-Bootcamp-Resources-main/section6/TDarrays.java
+++ Java-Bootcamp-Resources-main/section6/TDarrays.java
@@ -2,14 +2,23 @@ import java.util.Arrays;
 
 public class TDarrays{
     public static void main(String[] args) {
-        int[][] grades = {
-            {72,74,78,76},
-            {62,64,68,66},
-            {82,84,88,86}
-        };
-        
-        System.out.println("\tHarry: " + grades[0][0] + " " + grades[0][1] + " " +grades[0][2] + " " + grades[0][3]);
-        System.out.println("\tRon: " + grades[1][0] + " " + grades[1][1] + " " +grades[1][2] + " " + grades[1][3]);
-        System.out.println("\tHermione: " + grades[2][0] + " " + grades[2][1] + " " +grades[2][2] + " " + grades[2][3]);
+        int[][] grades = new int[3][4];
+        grades[0][0] = 72;
+        grades[0][1] = 74;
+        grades[0][2] = 78;
+        grades[0][3] = 76;
+
+        grades[1][0] = 62;
+        grades[1][1] = 64;
+        grades[1][2] = 68;
+        grades[1][3] = 66;
+
+        grades[2][0] = 82;
+        grades[2][1] = 84;
+        grades[2][2] = 88;
+        grades[2][3] = 86;
+        System.out.println("\tHarry: " + Arrays.toString(grades[0]));
+        System.out.println("\tRon: " + Arrays.toString(grades[1]));
+        System.out.println("\tHermione: " + Arrays.toString(grades[2]));
     }
 }
